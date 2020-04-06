9f272c4ad4981e24f6106b5096982f0c130f0302
yma010 <yma010@ucr.edu>
2020-04-06 16:35:42 -0700

    reorganized board into a class component

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -1,26 +1,41 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {Checkerboard} from './checkerboard';
 import './board.scss';
 
-export default function Board() {
-  const [ size, setSize ] = useState(8);
+export default class Board extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      size: 8,
+    }
+    this.handleChange.bind();
+    this.handleSubmit.bind();
+  }
   
-  const handleSubmit = e => {
+  handleSubmit = e => {
     e.preventDefault()
-
-    return(
-      Checkerboard(size)
-    )
+    this.setState({
+      size: e.currentTarget.value
+    })
   }
 
-  const handleChange = field => {
-    setSize(field.currentTarget.value)    
+  handleChange = field => {
+
+    this.setState({
+      [field] : field.currentTarget.value
+    });
   }
+
+  render(){
+
+    const {size} = this.state;
   return (
     <>
-      <form className="board-gen" onSubmit={handleSubmit}>
+      <form className="board-gen" onSubmit={this.handleSubmit()}>
         <label htmlFor='boardSize'>
-          Please input board size: <input id='boardSize' onChange={handleChange} type="number" value={size}/>
+          Please input board size: <input id='boardSize' onChange={this.handleChange()} type="number" value={size}/>
         </label>
         <button type="submit"> Generate New Board </button>
       </form>
@@ -28,5 +43,5 @@ export default function Board() {
         {Checkerboard(size)}
       </div>
     </>
-  )
+  )}
 }
